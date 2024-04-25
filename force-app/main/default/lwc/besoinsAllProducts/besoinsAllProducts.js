ad6a49b4ddae770c5b99435b631de8f4c8c11d08
GratiAbir <103220688+GratiAbir@users.noreply.github.com>
2024-04-25 11:33:15 +0100

    Besoins

--- force-app/main/default/lwc/besoinsAllProducts/besoinsAllProducts.js
+++ force-app/main/default/lwc/besoinsAllProducts/besoinsAllProducts.js
@@ -1,3 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getProductsBesoinsPage from '@salesforce/apex/productController.getProductsBesoinsPage';
 
-export default class BesoinsAllProducts extends LightningElement {}
+
+export default class BesoinsAllProducts extends LightningElement {
+    products;
+
+    @wire(getProductsBesoinsPage)
+    wiredProducts({ error, data }) {
+        if (data) {
+            this.products = data;
+            console.log(JSON.stringify(data));
+        } else if (error) {
+            console.error(error);
+        }
+    }
+}
